26cd4b1771d953130c5c9df90f006ed0be81d0f7
busyqqw <matsyta.g@gmail.com>
2023-10-17 21:01:24 +0300

    second test

---
+++ UnitTest2/UnitTest2.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../5.0/Source.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest2
+{
+	TEST_CLASS(UnitTest2)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int c;
+			c = h(1, 2);
+			Assert::AreEqual(c, 1, 0.0001);
+		}
+	};
+}
